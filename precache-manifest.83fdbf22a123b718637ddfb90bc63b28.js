6d82d4bcf9b6a8190f5e48665268a7f66fc55724
Max <maks.diacenko@mail.ru>
2020-08-24 14:32:04 +0300

    Updates

---
+++ precache-manifest.83fdbf22a123b718637ddfb90bc63b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c207fc1646dd123c9412568eada667e",
+    "url": "/http//%20maksimus12.github.io/todoapp/index.html"
+  },
+  {
+    "revision": "41ce0181faefddbfe262",
+    "url": "/http//%20maksimus12.github.io/todoapp/static/css/main.03cf6968.chunk.css"
+  },
+  {
+    "revision": "ed2e1647844c0f861b36",
+    "url": "/http//%20maksimus12.github.io/todoapp/static/js/2.6f680aff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//%20maksimus12.github.io/todoapp/static/js/2.6f680aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ce0181faefddbfe262",
+    "url": "/http//%20maksimus12.github.io/todoapp/static/js/main.3b5fa716.chunk.js"
+  },
+  {
+    "revision": "e574f8c497e63e90a218",
+    "url": "/http//%20maksimus12.github.io/todoapp/static/js/runtime-main.ce1ee41b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todoapp/precache-manifest.a6ad29cabd5e885b08af8a605a7d12a2.js"
+  "/http//%20maksimus12.github.io/todoapp/precache-manifest.83fdbf22a123b718637ddfb90bc63b28.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todoapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/http//%20maksimus12.github.io/todoapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
